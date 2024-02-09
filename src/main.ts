a3c648f4d315867aaef1db70b7356d83ddb54489
Gokturk Kose <gokturkbrk@gmail.com>
2024-02-09 14:39:02 +0100

    add primevue

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import i18n from './core/plugins/i18n'
+import PrimeVue from 'primevue/config';
 
 const pinia = createPinia()
 
-createApp(App).use(pinia).use(i18n).mount('#app')
+createApp(App).use(pinia).use(i18n).use(PrimeVue).mount('#app')
