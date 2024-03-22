0012313637f7b31fd32ff126734a2dff7b2f13b8
Gokturk Kose <gokturkbrk@gmail.com>
2024-03-22 11:35:59 +0100

    create ligfinder sidebar

---
+++ src/components/ligfinder/LIGSidebar.vue
@@ -0,0 +1,18 @@
+<template>
+	<SidebarLayout :id="sidebarID" position="left" :collapsed=false>
+	</SidebarLayout>
+</template>
+
+<script setup lang="ts">
+import SidebarLayout from "../SidebarLayout.vue";
+import { useMapStore } from "../../store/map";
+import { SidebarControl } from "../../core/helpers/sidebarControl";
+const mapStore = useMapStore()
+const sidebarID = "ligfinder-sidebar"
+
+const sidebarControl = new SidebarControl("", sidebarID, document.createElement("div"))
+mapStore.map.addControl(sidebarControl, "top-left")
+</script>
+
+<style scoped>
+</style>
