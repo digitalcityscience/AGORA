65c3bb4cb4ef33530f5fac4bee87e06619a9b708
Gokturk Kose <gokturkbrk@gmail.com>
2024-03-27 11:18:43 +0100

    add critera add and delete operations for storing criteria list

--- src/components/ligfinder/LIGCriteriaFilter.vue
+++ src/components/ligfinder/LIGCriteriaFilter.vue
@@ -4,6 +4,24 @@
 			<span class="font-bold">Properties Based Filtering</span>
 		</template>
 		<div>
+			<div class="used-criteria">
+				<div class="included py-1">
+					<div class="text-surface-700 dark:text-surface-0 font-bold w-full">
+						Included Criteria
+					</div>
+					<div class="w-full p-1">
+						<ChipWrapper v-for="crit in includedCriteria" :key="crit.key" :label="crit.label" @remove="removeFromAppliedCriteria(crit)" removable severity="success"/>
+					</div>
+				</div>
+				<div class="excluded py-1">
+					<div class="text-surface-700 dark:text-surface-0 font-bold w-full">
+						Excluded Criteria
+					</div>
+					<div class="w-full p-1">
+						<ChipWrapper v-for="crit in excludedCriteria" :key="crit.key" :label="crit.label" @remove="removeFromAppliedCriteria(crit)" removable severity="danger"/>
+					</div>
+				</div>
+			</div>
 			<Tree :value="criteria.list" :filter="true" filterMode="strict" class="w-full md:w-30rem">
 			<template #default="slotProps">
 				<div class="w-full flex justify-between">
@@ -12,8 +30,8 @@
 					</div>
 					<div v-if="slotProps.node.children?.length === 0">
 						<div class="actions">
-							<Button icon="pi pi-search-plus" text rounded aria-label="Include"></Button>
-							<Button icon="pi pi-search-minus" severity="danger" text rounded aria-label="Exclude"></Button>
+							<Button icon="pi pi-search-plus" @click="addToAppliedCriteria(slotProps.node,'included')" text rounded aria-label="Include"></Button>
+							<Button icon="pi pi-search-minus" @click="addToAppliedCriteria(slotProps.node,'excluded')" severity="danger" text rounded aria-label="Exclude"></Button>
 						</div>
 					</div>
 				</div>
@@ -27,10 +45,25 @@
 import Panel from "primevue/panel";
 import Tree from "primevue/tree";
 import Button from "primevue/button";
-import { useCriteriaStore } from "../../store/criteria"
+import ChipWrapper from "../ChipWrapper.vue"
+import { type AppliedCriteria, useCriteriaStore } from "../../store/criteria"
+import { type TreeNode } from "primevue/treenode";
+import { computed } from "vue";
 const criteria = useCriteriaStore()
+
+const includedCriteria = computed(()=> { return criteria.criteriaInUse.filter((crit)=> { return crit.status === "included" }) })
+const excludedCriteria = computed(()=> { return criteria.criteriaInUse.filter((crit)=> { return crit.status === "excluded" }) })
+
+type CriteriaStatus="included"|"excluded"
+function addToAppliedCriteria(node: TreeNode, stat: CriteriaStatus): void{
+    const criterium: AppliedCriteria = { ...node, status:stat }
+    criteria.addCriteria(criterium)
+}
+function removeFromAppliedCriteria(crit: AppliedCriteria): void {
+    criteria.removeCriteria(crit)
+}
 </script>
 
-	<style scoped>
+<style scoped>
 
-	</style>
+</style>
--- src/store/criteria.ts
+++ src/store/criteria.ts
@@ -1,10 +1,33 @@
 import { defineStore, acceptHMRUpdate } from "pinia"
 import criteria from "../../criteria.json"
+import { ref } from "vue";
+import { type TreeNode } from "primevue/treenode";
 
+export interface AppliedCriteria extends TreeNode {
+    status: "included"|"excluded"
+}
 export const useCriteriaStore = defineStore("criteria", () => {
     const list = criteria.items;
+    const criteriaInUse = ref<AppliedCriteria[]>([])
+    const addCriteria = (criteria: AppliedCriteria): void => {
+        if (criteriaInUse.value.filter((crit)=>{ return crit.key === criteria.key }).length === 0){
+            criteriaInUse.value.push(criteria)
+            console.log(criteriaInUse.value)
+        } else {
+            const index = criteriaInUse.value.findIndex(crit => crit.key === criteria.key)
+            if (criteriaInUse.value[index].status !== criteria.status) {
+                criteriaInUse.value[index].status = criteria.status
+            }
+        }
+    }
+    const removeCriteria = (criteria: AppliedCriteria): void => {
+        criteriaInUse.value = criteriaInUse.value.filter(c => c.key !== criteria.key)
+    }
     return {
-        list
+        list,
+        criteriaInUse,
+        addCriteria,
+        removeCriteria
     }
 })
 /* eslint-disable */
