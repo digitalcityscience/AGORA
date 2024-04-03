2c062e83c18528b947e160c907450e9c9ad15d7b
Gokturk Kose <gokturkbrk@gmail.com>
2024-04-03 14:42:28 +0200

    add parcel dataset on the map directly after map is mounted

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -7,8 +7,11 @@
 import maplibre from "maplibre-gl"
 import { onMounted } from "vue";
 import { useMapStore } from "../store/map";
+import { useGeoserverStore } from "../store/geoserver";
+import { isNullOrEmpty } from "../core/helpers/functions";
 
 const mapStore = useMapStore()
+const geoserver = useGeoserverStore()
 onMounted(() => {
     mapStore.map = new maplibre.Map({
         container: "map",
@@ -19,6 +22,37 @@ onMounted(() => {
     // Add zoom and rotation controls to the map.
     const zoomControl = new maplibre.NavigationControl()
     mapStore.map.addControl(zoomControl, "top-right");
+
+    geoserver.getLayerDetail(`${import.meta.env.VITE_PARCEL_DATASET_LAYERDETAIL_URL}`).then((detail) => {
+        if (!isNullOrEmpty(detail)) {
+            const dataType = (): string => {
+                const feature = detail.featureType.attributes.attribute.filter((att) => { return att.name === "geom" })
+                return feature.length > 0 ? feature[0].binding.split(".").slice(-1)[0] : ""
+            }
+            mapStore.addMapDataSource(
+                "geoserver",
+                detail.featureType.name,
+                false,
+                `${String(import.meta.env.VITE_PARCEL_DATASET_WORKSPACENAME)}`,
+                detail).then(() => {
+                if (!isNullOrEmpty(dataType) && !isNullOrEmpty(detail)) {
+                    mapStore.addMapLayer(
+                        "geoserver",
+                        detail.featureType.name,
+                        mapStore.geometryConversion(dataType()),
+                        undefined,
+                        detail,
+                        `${detail.featureType.name}`
+                    ).then(()=>{
+                    }).catch(error => {
+                        window.alert(error)
+                    })
+                }
+            }).catch(error => {
+                window.alert(error)
+            })
+        }
+    }).catch(err => { window.alert(err) })
 })
 </script>
 
