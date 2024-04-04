433a6c0ff41dab0e94870fe0050ce145a2115bcc
Gokturk Kose <gokturkbrk@gmail.com>
2024-04-04 11:01:23 +0200

    apply mbstyle to parcel dataset if present

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -6,7 +6,7 @@
 <script setup lang="ts">
 import maplibre from "maplibre-gl"
 import { onMounted } from "vue";
-import { useMapStore } from "../store/map";
+import { type LayerStyleOptions, useMapStore } from "../store/map";
 import { useGeoserverStore } from "../store/geoserver";
 import { isNullOrEmpty } from "../core/helpers/functions";
 
@@ -22,38 +22,67 @@ onMounted(() => {
     // Add zoom and rotation controls to the map.
     const zoomControl = new maplibre.NavigationControl()
     mapStore.map.addControl(zoomControl, "top-right");
+    // Add parcel dataset after map is ready.
+    loadParcelDataset().then(()=>{}, ()=>{})
+})
 
-    geoserver.getLayerDetail(`${import.meta.env.VITE_PARCEL_DATASET_LAYERDETAIL_URL}`).then((detail) => {
-        if (!isNullOrEmpty(detail)) {
-            const dataType = (): string => {
-                const feature = detail.featureType.attributes.attribute.filter((att) => { return att.name === "geom" })
-                return feature.length > 0 ? feature[0].binding.split(".").slice(-1)[0] : ""
-            }
-            mapStore.addMapDataSource(
-                "geoserver",
-                detail.featureType.name,
-                false,
-                `${String(import.meta.env.VITE_PARCEL_DATASET_WORKSPACENAME)}`,
-                detail).then(() => {
-                if (!isNullOrEmpty(dataType) && !isNullOrEmpty(detail)) {
-                    mapStore.addMapLayer(
-                        "geoserver",
-                        detail.featureType.name,
-                        mapStore.geometryConversion(dataType()),
-                        undefined,
-                        detail,
-                        `${detail.featureType.name}`
-                    ).then(()=>{
-                    }).catch(error => {
+async function loadParcelDataset(): Promise<void> {
+    const layerName: string = `${import.meta.env.VITE_PARCEL_DATASET_LAYERNAME}`
+    const layerInfoURL: string = `${import.meta.env.VITE_PARCEL_DATASET_LAYERINFORMATION_URL}`
+    if (layerName.length>0 && layerInfoURL.length>0){
+        geoserver.getLayerInformation({ name:"parcel3857", href:"http://dev.geoserver.tosca.dcs.hcu-hamburg.de/geoserver/rest/workspaces/public/layers/parcel3857.json" }, "public")
+            .then((layerInformation) => {
+                let layerStyling: LayerStyleOptions
+                // Currently we are just picking styles which has include mbstyle in name. Further optimization needed after some period
+                // TODO: remove mbstyle selector
+                if (layerInformation.layer.defaultStyle.href.includes("mbstyle")){
+                    const regex = /\.json\b/;
+                    const url = layerInformation.layer.defaultStyle.href.replace(regex, ".mbstyle")
+                    geoserver.getLayerStyling(url).then(style => {
+                        if (style.layers.length > 0){
+                            layerStyling = geoserver.convertLayerStylingToMaplibreStyle(style)
+                        }
+                    }).catch((error) => {
                         window.alert(error)
                     })
                 }
-            }).catch(error => {
-                window.alert(error)
-            })
-        }
-    }).catch(err => { window.alert(err) })
-})
+                if (layerInformation !== undefined) {
+                    geoserver.getLayerDetail(layerInformation.layer.resource.href).then((detail) => {
+                        if (!isNullOrEmpty(detail)) {
+                            const dataType = (): string => {
+                                const feature = detail.featureType.attributes.attribute.filter((att) => { return att.name === "geom" })
+                                return feature.length > 0 ? feature[0].binding.split(".").slice(-1)[0] : ""
+                            }
+                            mapStore.addMapDataSource(
+                                "geoserver",
+                                detail.featureType.name,
+                                false,
+                                `${String(import.meta.env.VITE_PARCEL_DATASET_WORKSPACENAME)}`,
+                                detail).then(() => {
+                                if (!isNullOrEmpty(dataType) && !isNullOrEmpty(detail)) {
+                                    mapStore.addMapLayer(
+                                        "geoserver",
+                                        detail.featureType.name,
+                                        mapStore.geometryConversion(dataType()),
+                                        !isNullOrEmpty(layerStyling) ? { ...layerStyling }: undefined,
+                                        detail,
+                                        `${detail.featureType.name}`
+                                    ).then(()=>{
+                                    }).catch(error => {
+                                        window.alert(error)
+                                    })
+                                }
+                            }).catch(error => {
+                                window.alert(error)
+                            })
+                        }
+                    }).catch(err => { window.alert(err) })
+                }
+            }).catch(err => { window.alert(err) })
+    } else {
+        window.alert("could not find parcel dataset information")
+    }
+}
 </script>
 
 <style scoped>
