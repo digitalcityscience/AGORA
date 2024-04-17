b144e9ce2de846aee5ef1e35d0c0b9b7cf9111f7
Gokturk Kose <gokturkbrk@gmail.com>
2024-04-17 10:52:10 +0200

    add administrative area selection operation to geometry filter

--- src/components/ligfinder/LIGGeometryFilter.vue
+++ src/components/ligfinder/LIGGeometryFilter.vue
@@ -1,16 +1,20 @@
 <template>
-<Panel toggleable>
-    <template #header>
-		<span class="font-bold">Location Based Filtering</span>
-	</template>
-	<div></div>
-</Panel>
+	<div class="w-full">
+		<Panel toggleable>
+			<template #header>
+				<span class="font-bold">Location Based Filtering</span>
+			</template>
+			<div class="w-full flex flex-col">
+				<LIGGeometryAdministrativeAreas/>
+			</div>
+		</Panel>
+	</div>
 </template>
 
 <script setup lang="ts">
 import Panel from "primevue/panel";
+import LIGGeometryAdministrativeAreas from "./LIGGeometryAdministrativeAreas.vue"
 </script>
 
 <style scoped>
-
 </style>
