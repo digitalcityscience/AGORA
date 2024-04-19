8af3c798c18fb78ed6dd66e5050170c3a18ad2a9
Gokturk Kose <gokturkbrk@gmail.com>
2024-04-19 15:07:20 +0200

    add css for small screens

--- src/components/ligfinder/LIGGeometryIsochroneAreas.vue
+++ src/components/ligfinder/LIGGeometryIsochroneAreas.vue
@@ -1,40 +1,51 @@
 <template>
-	<Card class="w-full">
-		<template #title>Isochrone Areas</template>
-		<template #content>
-			<div class="w-full py-1" v-if="geometry.selectedIsochrone.length>0">
-				<ChipWrapper v-for="(_isochrone,index) in geometry.selectedIsochrone" :key="index" label="Isochrone" @remove="geometry.removeSelectedIsochrone" removable severity="success"/>
-			</div>
-			<div class="w-full flex justify-between p-1">
-				<Button v-if="!selectionOnProgress" size="small" class="text-xs leading-4 grow-0" @click="startCenterSelection">
-					<template #icon>
-						<i class="material-icons">add_location_alt</i>
-					</template>
-				</Button>
-				<Button v-else size="small" class="text-xs leading-4 grow-0" @click="cancelCenterSelection">
-					<template #icon>
-						<i class="material-icons">wrong_location</i>
-					</template>
-				</Button>
-				<SelectButton class="grow-0" v-model="selectedTravelMode" :options="traveModeList" optionLabel="name"
-					dataKey="value" aria-labelledby="custom">
-					<template #option="slotProps">
-						<i class="material-icons">{{ slotProps.option.icon }}</i>
-					</template>
-				</SelectButton>
-				<InputNumber v-model="travelTime" input-class="grow" suffix="min" :min="0"></InputNumber>
-				<Button class="grow-0" :disabled="centerPoint === undefined" @click="createIsochrone">Create</Button>
-			</div>
-			<div class="w-full grid grid-cols-3 pt-1" v-if="isochroneOnTheMap && isochroneOnTheMapData">
-                <div class="p-1">
-                    <Button class="w-full" size="small" @click="addSelectedIsochrone(isochroneOnTheMapData)">Add to list</Button>
+    <Card class="w-full isochrone-filter">
+        <template #title>Isochrone Areas</template>
+        <template #content>
+            <div class="w-full py-1" v-if="geometry.selectedIsochrone.length > 0">
+                <ChipWrapper v-for="(_isochrone, index) in geometry.selectedIsochrone" :key="index" label="Isochrone"
+                    @remove="geometry.removeSelectedIsochrone" removable severity="success" />
+            </div>
+            <div class="w-full 2xl:flex 2xl:justify-between 2xl:grid-cols-none lg:grid lg:grid-cols-4 lg:gap-2 2xl:gap-0 p-1 ">
+                <div class="">
+                    <Button v-if="!selectionOnProgress" size="small" class="text-xs leading-4 2xl:grow-0 lg:w-full"
+                        @click="startCenterSelection">
+                        <template #icon>
+                            <i class="material-icons">add_location_alt</i>
+                        </template>
+                    </Button>
+                    <Button v-else size="small" class="text-xs leading-4 2xl:grow-0" @click="cancelCenterSelection">
+                        <template #icon>
+                            <i class="material-icons">wrong_location</i>
+                        </template>
+                    </Button>
                 </div>
-                <div class="p-1">
+                <div class="lg:grid lg:grid-cols-subgrid lg:col-span-3 2xl:grid-cols-none">
+                    <SelectButton class="2xl:grow-0 lg:col-span-3 travel-mode flex" v-model="selectedTravelMode" :options="traveModeList"
+                        optionLabel="name" dataKey="value" aria-labelledby="custom">
+                        <template #option="slotProps">
+                            <i class="material-icons">{{ slotProps.option.icon }}</i>
+                        </template>
+                    </SelectButton>
+                </div>
+                <div class="lg:grid lg:grid-cols-subgrid lg:col-span-4 2xl:grid-cols-none">
+                    <InputNumber v-model="travelTime" input-class="grow lg:col-span-4" suffix="min" :min="0"></InputNumber>
+                </div>
+                <div class="lg:grid lg:grid-cols-subgrid lg:col-span-4 2xl:grid-cols-none">
+                    <Button class="lg:col-span-4 2xl:grow-0" :disabled="centerPoint === undefined" @click="createIsochrone">Create</Button>
+                </div>
+            </div>
+            <div class="w-full grid grid-cols-4 pt-1" v-if="isochroneOnTheMap && isochroneOnTheMapData">
+                <div class="p-1 lg:col-span-2">
+                    <Button class="w-full" size="small" @click="addSelectedIsochrone(isochroneOnTheMapData)">Add to
+                        list</Button>
+                </div>
+                <div class="p-1 lg:col-span-2">
                     <Button class="w-full" size="small" @click="cancelIsochroneSelection()">Cancel</Button>
                 </div>
-			</div>
-		</template>
-	</Card>
+            </div>
+        </template>
+    </Card>
 </template>
 
 <script setup lang="ts">
@@ -77,29 +88,29 @@ function createIsochrone(): void {
     if (centerPoint.value === undefined) {
         console.error("There is no center point")
     }
-    if (!(travelTime.value>0)){
+    if (!(travelTime.value > 0)) {
         console.error("There is not enough travel time")
     }
-    if (centerPoint.value?.lat !== undefined && centerPoint.value.lng !== undefined){
+    if (centerPoint.value?.lat !== undefined && centerPoint.value.lng !== undefined) {
         const isochroneInfo: IsochroneForm = {
             time: travelTime.value,
             center: { ...centerPoint.value },
             mode: selectedTravelMode.value.value
         }
-        geometry.getIsochrone(isochroneInfo).then((response)=>{
+        geometry.getIsochrone(isochroneInfo).then((response) => {
             addIsochroneSource(response)
-        }).catch((error=>{ console.error(error); })).finally(()=>{
+        }).catch((error => { console.error(error); })).finally(() => {
             cancelCenterSelection()
         })
     }
 }
-function startCenterSelection(): void{
+function startCenterSelection(): void {
     selectionOnProgress.value = true
     centerSelectDrawer.start()
     centerSelectDrawer.setMode("point")
     centerSelectDrawer.on("change", centerSelector)
 }
-function cancelCenterSelection(): void{
+function cancelCenterSelection(): void {
     selectionOnProgress.value = false
     centerSelectDrawer.setMode("static")
     centerSelectDrawer.off("change", centerSelector)
@@ -107,31 +118,31 @@ function cancelCenterSelection(): void{
     centerPoint.value = undefined
     travelTime.value = 0
 }
-function centerSelector(): void{
+function centerSelector(): void {
     const snap = centerSelectDrawer.getSnapshot()
-    if (snap.length>1){
+    if (snap.length > 1) {
         centerSelectDrawer.removeFeatures([snap[0].id ?? ""])
     }
-    centerPoint.value = { ...{ lng:snap[0].geometry.coordinates[0] as number, lat:snap[0].geometry.coordinates[1] as number } }
+    centerPoint.value = { ...{ lng: snap[0].geometry.coordinates[0] as number, lat: snap[0].geometry.coordinates[1] as number } }
 }
 const isochroneOnTheMap = ref<boolean>(false)
 const isochroneOnTheMapData = ref<FeatureCollection>()
-function addIsochroneSource(src: FeatureCollection): void{
-    const layerStyle = { paint:{ "fill-color":"#abcdef", "fill-opacity":1 } }
-    mapStore.addMapDataSource("geojson", "isochrone-temp-source", false, undefined, undefined, src).then(()=>{
-        mapStore.addMapLayer("geojson", "isochrone-temp-source", "fill", layerStyle, undefined, undefined, src).then(()=>{
+function addIsochroneSource(src: FeatureCollection): void {
+    const layerStyle = { paint: { "fill-color": "#abcdef", "fill-opacity": 1 } }
+    mapStore.addMapDataSource("geojson", "isochrone-temp-source", false, undefined, undefined, src).then(() => {
+        mapStore.addMapLayer("geojson", "isochrone-temp-source", "fill", layerStyle, undefined, undefined, src).then(() => {
             isochroneOnTheMap.value = true
             isochroneOnTheMapData.value = src
-        }).catch((error)=>{ console.log(error) })
-    }).catch((error)=>{ console.error(error) })
+        }).catch((error) => { console.log(error) })
+    }).catch((error) => { console.error(error) })
 }
-function addSelectedIsochrone(data: FeatureCollection): void{
+function addSelectedIsochrone(data: FeatureCollection): void {
     const applied = geometry.addToSelectedIsochrone(data)
     if (applied) {
         cancelIsochroneSelection()
     }
 }
-function cancelIsochroneSelection(): void{
+function cancelIsochroneSelection(): void {
     mapStore.map.removeLayer("isochrone-temp-source")
     mapStore.removeFromLayerList("isochrone-temp-source")
     mapStore.map.removeSource("isochrone-temp-source")
@@ -139,4 +150,10 @@ function cancelIsochroneSelection(): void{
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+@media screen and (max-width: 1440px){
+    .isochrone-filter div.travel-mode:deep(div.h-full) {
+        width: 100%;
+    }
+}
+</style>
