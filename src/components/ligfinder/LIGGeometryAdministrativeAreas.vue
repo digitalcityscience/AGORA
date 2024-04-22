ef8f87fd8a251784444a0a176f7330daace38036
Gokturk Kose <gokturkbrk@gmail.com>
2024-04-22 15:13:40 +0200

    add active layer to the map

--- src/components/ligfinder/LIGGeometryAdministrativeAreas.vue
+++ src/components/ligfinder/LIGGeometryAdministrativeAreas.vue
@@ -16,10 +16,11 @@
                 </div>
             <div class="w-full flex flex-col py-1">
                 <div class="w-full py-1">
-                    <Dropdown class="min-w-32" v-model="activeAdministrativeArea"
+                    <Dropdown class="min-w-32" v-model="geometry.activeAdministrativeArea"
                         :options="geometry.administrativeBoundariesList" option-label="name"
-                        :option-disabled="(option) => { return option.name === activeAdministrativeArea?.name }"
-                        placeholder="Please select" checkmark show-clear>
+                        :option-disabled="(option) => { return option.name === geometry.activeAdministrativeArea?.name }"
+                        placeholder="Please select" checkmark show-clear
+                        @change="changeActiveAdminLayerOnMap()">
                         <template #value="slotProps">
                             <div v-if="slotProps.value" class="flex align-items-center">
                                 <div>{{ slotProps.value.name }}</div>
@@ -30,7 +31,7 @@
                         </template>
                     </Dropdown>
                 </div>
-                <div class="w-full py-1" v-if="activeAdministrativeArea">
+                <div class="w-full py-1" v-if="geometry.activeAdministrativeArea">
                     <DataTable v-model:filters="featureListFilters" :value="activeAdministrativeAreaFeaturesList"
                         scrollable scrollHeight="300px" size="small"
                         :pt="{ thead: { class: ['hidden'] } }">
@@ -63,7 +64,7 @@
 import Card from "primevue/card"
 import Dropdown from "primevue/dropdown"
 import { onMounted, ref, computed, type Ref } from "vue";
-import { type AdministrativeBoundariesListItem, useGeometryStore, type AdministrativeFeature } from "../../store/ligfinder/geometry";
+import { useGeometryStore, type AdministrativeFeature } from "../../store/ligfinder/geometry";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import Button from "primevue/button";
@@ -73,6 +74,7 @@ import InputIcon from "primevue/inputicon"
 import SelectButton from "primevue/selectbutton";
 import ChipWrapper from "../ChipWrapper.vue";
 import { FilterMatchMode } from "primevue/api";
+import { type FeatureCollection } from "geojson";
 
 const geometry = useGeometryStore()
 onMounted(() => {
@@ -81,7 +83,6 @@ onMounted(() => {
             if (list.data.length > 0 && geometry.administrativeDataList?.length !== list.data.length) {
                 geometry.administrativeBoundariesList = [...list.data]
                 list.data.forEach(item => {
-                    console.log(item);
                     geometry.getAdministrativeBoundaryData(item.id).then((boundaryData) => {
                         geometry.administrativeDataList.push({ ...item, data: boundaryData })
                     }).catch((error) => { console.error(error) })
@@ -91,13 +92,13 @@ onMounted(() => {
     }
 })
 const activeAdministrativeAreaFeaturesList: Ref<AdministrativeFeature[]> = computed(() => {
-    if (activeAdministrativeArea.value == null) {
+    if (geometry.activeAdministrativeArea == null) {
         return []
     } else {
-        const activeDataFeaturesList = geometry.administrativeDataList.filter((item) => { return item.table_name === activeAdministrativeArea.value?.table_name })
+        const activeDataFeaturesList = geometry.administrativeDataList.filter((item) => { return item.table_name === geometry.activeAdministrativeArea?.table_name })
         if (activeDataFeaturesList.length > 0) {
             const options: AdministrativeFeature[] = []
-            activeDataFeaturesList[0].data.features.forEach(feature => options.push({ data:{ ...feature }, id:activeAdministrativeArea.value!.id, name:activeAdministrativeArea.value!.name, table_name:activeAdministrativeArea.value!.table_name }))
+            activeDataFeaturesList[0].data.features.forEach(feature => options.push({ data:{ ...feature }, id:geometry.activeAdministrativeArea!.id, name:geometry.activeAdministrativeArea!.name, table_name:geometry.activeAdministrativeArea!.table_name }))
             if (!(typeof options[0].data.properties!.name === "number")){
                 options.sort((a, b) => {
                     const nameA = String(a.data.properties!.name).toLowerCase();
@@ -121,7 +122,6 @@ const activeAdministrativeAreaFeaturesList: Ref<AdministrativeFeature[]> = compu
 const featureListFilters = ref({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS }
 })
-const activeAdministrativeArea = ref<AdministrativeBoundariesListItem | null>(null)
 
 function addToSelectedGeometries(data: AdministrativeFeature): void{
     try {
@@ -135,6 +135,19 @@ function removeFromSelectedGeometries(data: AdministrativeFeature): void {
         geometry.removeFromSelectedAdministrativeFeaturesList(data)
     } catch (error){
         console.error(error)
+    }
+}
+function changeActiveAdminLayerOnMap(): void{
+    console.log("current layer is: ", geometry.activeAdministrativeArea?.table_name)
+    if (geometry.activeAdministrativeArea !== null) {
+        const activeData = geometry.administrativeDataList.filter((item) => { return item.table_name === geometry.activeAdministrativeArea?.table_name })
+        geometry.updateActiveAdminLayer(activeData[0].data)
+    } else {
+        const emptyGeojson: FeatureCollection = {
+            type: "FeatureCollection",
+            features:[]
+        }
+        geometry.updateActiveAdminLayer(emptyGeojson)
     }
 }
 </script>
