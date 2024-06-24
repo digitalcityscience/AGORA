71a75f7bec0eea1e1843cb234b44d0ef35939496
Gokturk Kose <gokturkbrk@gmail.com>
2024-06-24 16:16:27 +0200

    add number formatter function

--- src/core/helpers/functions.ts
+++ src/core/helpers/functions.ts
@@ -26,3 +26,25 @@ export function isNullOrEmpty(arg: any): boolean{
         return true
     }
 }
+/**
+ * Formats a given number by separating thousands with periods (".") and
+ * decimals with commas (","), and limiting the result to two decimal places.
+ *
+ * @param num - The number to format.
+ * @returns The formatted number as a string.
+ *
+ * @example
+ * ```typescript
+ * const formattedNumber = formatNumber(1234567.8912);
+ * console.log(formattedNumber); // Output: "1.234.567,89"
+ * ```
+ */
+export function formatNumber(num: number): string {
+    if (isNaN(num)) {
+        return String(num);
+    }
+    const fixedNum = num.toFixed(2);
+    const [integerPart, decimalPart] = fixedNum.split(".");
+    const integerWithThousandsSeparator = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    return `${integerWithThousandsSeparator},${decimalPart}`;
+}
