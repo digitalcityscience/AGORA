08d3a4c138edab435896f8dacb626bbd28266918
Gokturk <gokturkbrk@gmail.com>
2025-04-10 14:42:39 +0200

    feat: Update domain structure and criteria filter implementation
    new criteria file structure affects whole expression build strategy. Also some logic bugs fixed with new approach.

---
+++ src/domains.ts
@@ -0,0 +1,34 @@
+import domains from "../domain_structure.json";
+
+export interface Domain {
+    meta: string;
+    data: Array<LGBArtData | NutzungDomainData>;
+}
+export interface LGBArtData {
+    key: string;
+    art: string[];
+    label: string;
+    label_en: string;
+    children: LGBTypData[];
+}
+export interface LGBTypData {
+    key: string;
+    art: string[];
+    typ: string[];
+    label: string;
+    label_en: string;
+}
+export interface NutzungDomainData {
+    key: string
+    nutzungvalue: string;
+    label: string;
+    label_en: string;
+    children: NutzungItemData[];
+}
+export interface NutzungItemData {
+    key: string;
+    nutzungvalue: string[];
+    label: string;
+    label_en: string;
+}
+export default domains as unknown as Domain;
